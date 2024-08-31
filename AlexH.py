425689a152a6c3f807e12bc606dc23b03f9668cb
Harwale069 <120582454+Harwale069@users.noreply.github.com>
2024-08-31 15:05:14 -0400

    Add files via upload

---
+++ AlexH.py
@@ -0,0 +1,68 @@
+import time
+import os
+import sys
+
+# Define the ASCII art for "Made by Alex Harwood"
+ascii_text = """
+\033[97m
+ __  __           _        _                _    _           
+|  \/  | __ _  __| | ___  | |__  _   _     / \\  | | _____  __
+| |\/| |/ _` |/ _` |/ _ \\ | '_ \\| | | |   / _ \\ | |/ _ \\ \\/ /
+| |  | | (_| | (_| |  __/ | |_) | |_| |  / ___ \\| |  __/>  < 
+|_|  |_|\__,_|\__,_|\___| |_.__/ \\__, | /_/   \\_\\_|\\___/_/\\_\\
+| | | | __ _ _ ____      _____   |___/ __| |                 
+| |_| |/ _` | '__\\ \\ / / _ \\ / _ \\ / _` |                 
+|  _  | (_| | |   \\ V  V / (_) | (_) | (_| |                 
+|_| |_|\__,_|_|    \\_/\\_/ \\___/ \\___/ \\__,_|                  
+\033[0m
+"""
+
+# Define the ASCII art for Walter White
+walter_white = """
+\033[97m
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢺⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠆⠜⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⠿⠿⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⣿⣿⣿⣿⣿
+⣿⣿⡏⠁⠀⠀⠀⠀⠀⣀⣠⣤⣤⣶⣶⣶⣶⣶⣦⣤⡄⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿
+⣿⣿⣷⣄⠀⠀⠀⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⡧⠇⢀⣤⣶⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣾⣮⣭⣿⡻⣽⣒⠀⣤⣜⣭⠐⢐⣒⠢⢰⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣏⣿⣿⣿⣿⣿⣿⡟⣾⣿⠂⢈⢿⣷⣞⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣿⣷⣶⣾⡿⠿⣿⠗⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠻⠋⠉⠑⠀⠀⢘⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⡿⠟⢹⣿⣿⡇⢀⣶⣶⠴⠶⠀⠀⢽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⡿⠀⠀⢸⣿⣿⠀⠀⠣⠀⠀⠀⠀⠀⡟⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⡿⠟⠋⠀⠀⠀⠀⠹⣿⣧⣀⠀⠀⠀⠀⡀⣴⠁⢘⡙⢿⣿⣿⣿⣿⣿⣿⣿⣿
+⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⠗⠂⠄⠀⣴⡟⠀⠀⡃⠀⠉⠉⠟⡿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢷⠾⠛⠂⢹⠀⠀⠀⢡⠀⠀⠀⠀⠀⠙⠛⠿⢿
+\033[0m
+"""
+
+# Define the color codes
+colors = [
+    "\033[91m",  # Red
+    "\033[92m",  # Green
+    "\033[93m",  # Yellow
+    "\033[94m",  # Blue
+    "\033[95m",  # Magenta
+    "\033[96m",  # Cyan
+    "\033[97m",  # White
+    "\033[0m"    # Reset
+]
+
+def clear_screen():
+    # Clear the terminal screen
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def print_text_and_art():
+    while True:
+        for color in colors:
+            clear_screen()
+            # Print the ASCII art text and Walter White ASCII art in the current color
+            sys.stdout.write(color + ascii_text + "\n" + walter_white + "\033[0m")
+            sys.stdout.flush()
+            time.sleep(1)  # Adjust the speed as needed
+
+if __name__ == "__main__":
+    print_text_and_art()
