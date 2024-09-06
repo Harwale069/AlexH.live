dda37c4ea627656a92ebbdbab97020b3c596d0a7
Harwale069 <120582454+Harwale069@users.noreply.github.com>
2024-09-06 14:35:11 -0400

    Update AlexH.py

--- AlexH.py
+++ AlexH.py
@@ -56,10 +56,10 @@ def walter_white_animation():
 ⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⠗⠂⠄⠀⣴⡟⠀⠀⡃⠀⠉⠉⠟⡿⣿⣿⣿⣿
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢷⠾⠛⠂⢹⠀⠀⠀⢡⠀⠀⠀⠀⠀⠙⠛⠿⢿
     """
-    for color in itertools.cycle([31, 32, 33, 34, 35, 36]):
+    for color in itertools.cycle([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,,24,25,26,27,28,29,30,31]):
         clear_screen()
         print(f"\033[38;5;{color}m{frame}\033[0m")
-        time.sleep(0.5)
+        time.sleep(0.1)
 
 # Main function to run the animation
 def main():
