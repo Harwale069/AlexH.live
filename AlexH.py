09c22b6cb97da5dfdf11f27fa3a5e0224d2be4e0
Harwale069 <120582454+Harwale069@users.noreply.github.com>
2024-09-20 14:09:43 -0400

    Update AlexH.py

--- AlexH.py
+++ AlexH.py
@@ -1,43 +1,14 @@
+import itertools
 import time
 import os
-import itertools
 
 # Function to clear the screen
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-# Function to print the text "Made by Alex Harwood"
-def print_text():
-    text = r"""
- __  __           _        _                _    _           
-|  \/  | __ _  __| | ___  | |__  _   _     / \  | | _____  __
-| |\/| |/ _` |/ _` |/ _ \ | '_ \| | | |   / _ \ | |/ _ \ \/ /
-| |  | | (_| | (_| |  __/ | |_) | |_| |  / ___ \| |  __/>  < 
-|_|  |_|\__,_|\__,_|\___| |_.__/ \__, | /_/   \_\_|\___/_/\_\
-| | | | __ _ _ ____      _____   |___/ __| |                 
-| |_| |/ _` | '__\ \ /\ / / _ \ / _ \ / _` |                 
-|  _  | (_| | |   \ V  V / (_) | (_) | (_| |                 
-|_| |_|\__,_|_|    \_/\_/ \___/ \___/ \__,_|                 
-    """
-    print(text)
-
-# Function to print bullet animation shooting down
-def bullet_animation():
-    frames = [
-        "•",
-        "•\n•",
-        "•\n•\n•",
-        "•\n•\n•\n•",
-        "•\n•\n•\n•\n•",
-    ]
-    for frame in frames:
-        clear_screen()
-        print(frame)
-        time.sleep(0.2)
-
-# Function to print the Walter White ASCII art with color change
+# Function to print the Walter White ASCII art with extended color cycling
 def walter_white_animation():
-    colors = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]  # Red, Green, Yellow, Blue, Magenta, Cyan
+    colors = list(range(30, 38)) + list(range(90, 98))  # Standard and bright color ranges
     frame = r"""
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
 ⣿⣿⣿⣿⣿⣿⣿⣿⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
@@ -45,7 +16,7 @@ def walter_white_animation():
 ⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠆⠜⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
 ⣿⣿⣿⣿⠿⠿⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⣿⣿⣿⣿⣿
 ⣿⣿⡏⠁⠀⠀⠀⠀⠀⣀⣠⣤⣤⣶⣶⣶⣶⣶⣦⣤⡄⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿
-⣿⣿⣷⣄⠀⠀⠀⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⡧⠇⢀⣤⣶⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣷⣄⠀⠀⠀⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⡧⠇⢀⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿
 ⣿⣿⣿⣿⣿⣿⣾⣮⣭⣿⡻⣽⣒⠀⣤⣜⣭⠐⢐⣒⠢⢰⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿
 ⣿⣿⣿⣿⣿⣿⣿⣏⣿⣿⣿⣿⣿⣿⡟⣾⣿⠂⢈⢿⣷⣞⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿
 ⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣿⣷⣶⣾⡿⠿⣿⠗⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
@@ -56,17 +27,10 @@ def walter_white_animation():
 ⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⠗⠂⠄⠀⣴⡟⠀⠀⡃⠀⠉⠉⠟⡿⣿⣿⣿⣿
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢷⠾⠛⠂⢹⠀⠀⠀⢡⠀⠀⠀⠀⠀⠙⠛⠿⢿
     """
-    for color in itertools.cycle([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,,24,25,26,27,28,29,30,31]):
+    for color in itertools.cycle(colors):
         clear_screen()
-        print(f"\033[38;5;{color}m{frame}\033[0m")
-        time.sleep(0.1)
+        print(f"\033[{color}m{frame}\033[0m")
+        time.sleep(0.5)
 
-# Main function to run the animation
-def main():
-    print_text()
-    time.sleep(5)  # Show the text for 5 seconds
-    bullet_animation()
-    walter_white_animation()
-
-if __name__ == "__main__":
-    main()
+# Example of how to use the function
+walter_white_animation()
